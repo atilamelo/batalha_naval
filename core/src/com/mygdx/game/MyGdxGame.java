6e434a2fc973ad9921a0dd943f473ed36cac3702
Atila <atilamelodesouza@gmail.com>
2023-03-11 15:48:40 -0300

    Arquivos iniciais gerado pela LibGDX

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,20 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {	
+	@Override
+	public void create () {
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(255, 255, 255, 1);
+
+	}
+	
+	@Override
+	public void dispose () {
+	}
+}
